25dc460062bacae040ce0c2eb3e0bdf571d00318
Stan Le Punk <stanislasiii@gmail.com>
2020-01-15 18:06:04 +0100

    printf

--- main_printf.c
+++ main_printf.c
@@ -1,40 +1,38 @@
-#include "ft_printf.h"
+#include “includes/libftprintf.h”
 #include <stdio.h>
-
 int	main(){
-	// ft_printf("\n");
-	// printf("\n");
+	// ft_printf(“\n”);
+	// printf(“\n”);
 
-	// ft_printf("%%\n");
-	// printf("%%\n");
+	// ft_printf(“%%\n”);
+	// printf(“%%\n”);
 
-	// ft_printf("%d\n", 42);
-	// printf("%d\n", 42);
+	// ft_printf(“%d\n”, 42);
+	// printf(“%d\n”, 42);
 
-	// ft_printf("%i\n", 2147483647);
-	// printf("%i\n", 2147483647);
+	// ft_printf(“%i\n”, 2147483647);
+	// printf(“%i\n”, 2147483647);
 
-	// ft_printf("%zd\n", 4294967295);
-	// printf("%zd\n", 4294967295);
+	// ft_printf(“%zd\n”, 4294967295);
+	// printf(“%zd\n”, 4294967295);
 
-	// ft_printf("%s\n", "Alors cette lib bien");
-	// printf("%s\n", "Alors cette lib bien");
+	// ft_printf(“%10s\n”, “Alors”);
+	// printf(“%10s\n”, “Alors”);
 
-	// ft_printf("%s\n", L"ݗݜशব");
-	// printf("%s\n", L"ݗݜशব");
+	// ft_printf(“%s\n”, L”ݗݜशব“);
+	// printf(“%s\n”, L”ݗݜशব“);
 
-	// ft_printf("\033[36m");
-	// printf("\033[36m");
+	// ft_printf(“\033[36m”);
+	// printf(“\033[36m”);
 
-	// ft_printf("%-10i\n", 3);
-	// printf("%-10i\n", 3);
+	// ft_printf(“%-10i”, 3);
+	// printf(“%-10i\n”, 3);
 
-	// ft_printf("%s\n", NULL);
-	// printf("%s\n", NULL);
-
-
-  
-
+	// ft_printf(“%s\n”, NULL);
+	// printf(“%s\n”, NULL);
+	
+	ft_printf(“%u\n”, -3);
+	printf(“%u\n”, -3);
   // while (1);
   return 0;
 }
