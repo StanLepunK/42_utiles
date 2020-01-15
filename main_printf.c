7475a53680b1ccd5afa604b3f2fcecc34f467bc4
Stan Le Punk <stanislasiii@gmail.com>
2020-01-15 15:08:26 +0100

    printf

---
+++ main_printf.c
@@ -0,0 +1,40 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+int	main(){
+	// ft_printf("\n");
+	// printf("\n");
+
+	// ft_printf("%%\n");
+	// printf("%%\n");
+
+	// ft_printf("%d\n", 42);
+	// printf("%d\n", 42);
+
+	// ft_printf("%i\n", 2147483647);
+	// printf("%i\n", 2147483647);
+
+	// ft_printf("%zd\n", 4294967295);
+	// printf("%zd\n", 4294967295);
+
+	// ft_printf("%s\n", "Alors cette lib bien");
+	// printf("%s\n", "Alors cette lib bien");
+
+	// ft_printf("%s\n", L"ݗݜशব");
+	// printf("%s\n", L"ݗݜशব");
+
+	// ft_printf("\033[36m");
+	// printf("\033[36m");
+
+	// ft_printf("%-10i\n", 3);
+	// printf("%-10i\n", 3);
+
+	// ft_printf("%s\n", NULL);
+	// printf("%s\n", NULL);
+
+
+  
+
+  // while (1);
+  return 0;
+}
