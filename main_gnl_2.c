c735f3f4ab24588ab4aa359a3c90c17150b475a0
Stan Le Punk <stanislasiii@gmail.com>
2020-01-17 10:45:43 +0100

    gnl

---
+++ main_gnl_2.c
@@ -0,0 +1,27 @@
+#include "get_next_line.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+# include "libft.h"
+
+
+int main() {
+	int fd = open("voyage.txt",O_RDONLY);
+	int count = 0;
+	char *line;
+	while (get_next_line(fd, &line) > 0) {
+		printf("ligne %i %s\n",count,line);
+		free(line);
+		count++;
+	}
+	printf("ligne %i %s\n",count,line);
+	free(line);
+	close(fd);
+	return 0;
+}
+
+
